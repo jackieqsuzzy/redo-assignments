73d90a5592c8de6222e923a27367a6367a1712f5
jackieqsuzzy <61442030+jackieqsuzzy@users.noreply.github.com>
2022-02-15 22:37:31 -0700

    updating changes

---
+++ level-1/loops-olympics/app.js
@@ -0,0 +1,51 @@
+// 1. Write a for loop that prints to the console the numbers 0 through 9.
+
+// for (let i = 0; i < 10; i++){
+// console.log(i)
+// }
+
+// 2. Write a for loop that prints to the console 9 through 0.
+// for (let i = 9; i > 0; i--){
+//     console.log(i)
+// }
+
+// 3. Write a for loop that prints these fruits to the console.
+
+// const fruit = ["banana", "orange", "apple", "kiwi"]
+
+// for (let i = 0; i < fruit.length; i++){
+//     console.log(fruit[i])
+// }
+// const item is the same as let i = 0
+//  you us "of" to go a array 
+//  you us "in" to go over an object
+// for (const [index, item] of fruit.entries()){
+//     console.log(index,item)
+// }
+
+// fruit.forEach((item, index) => {
+//     console.log(index,item)
+// })
+
+
+// 4. Write a for loop that will push the numbers 0 through 9 to an array.
+
+// var numbers = []
+
+// for (let i = 1; i <= 9; i++){
+//     numbers.push(i)
+// }
+
+// console.log(numbers)
+
+
+
+
+// // 5. Write a for loop that prints to the console only even numbers 0 through 100.
+
+// for (let i = 0; i <= 101; i++){
+//     if 
+// }
+
+// 3. Write a for loop that will push every other fruit to an array.`const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]`
+
---
+++ level-1/querySelector-Practice/app.js
@@ -0,0 +1,6 @@
+
+
+var result = document.getElementById("fav-dogfoods")
+for (var i = 0; i< result.length; i++) {
+    result[i].textContent = "** REDACTED **"
+}
