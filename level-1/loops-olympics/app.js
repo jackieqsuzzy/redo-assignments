5ff5ee0e7ae0f80c377995ba09aea07d7a08683f
jackieqsuzzy <61442030+jackieqsuzzy@users.noreply.github.com>
2022-02-16 15:08:35 -0700

    adding new changes

--- level-1/loops-olympics/app.js
+++ level-1/loops-olympics/app.js
@@ -27,8 +27,9 @@
 //     console.log(index,item)
 // })
 
+// Bronze Metal
 
-// 4. Write a for loop that will push the numbers 0 through 9 to an array.
+// 1. Write a for loop that will push the numbers 0 through 9 to an array.
 
 // var numbers = []
 
@@ -41,11 +42,95 @@
 
 
 
-// // 5. Write a for loop that prints to the console only even numbers 0 through 100.
+// // 2. Write a for loop that prints to the console only even numbers 0 through 100.
 
 // for (let i = 0; i <= 101; i++){
-//     if 
+//     if (i % 2 === 0){
+//         console.log(i)
+//     }
 // }
 
-// 3. Write a for loop that will push every other fruit to an array.`const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]`
+// 3. Write a for loop that will push every other fruit to an array.
 
+// const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+
+
+// var everyOther = []
+
+// for ( var i = 0; i < fruit.length; i += 2){
+//     everyOther.push(fruit[i])
+// }
+
+// console.log(everyOther)
+
+
+// Silver Metal 
+
+const peopleArray = [
+    {
+      name: "Harrison Ford",
+      occupation: "Actor"
+    },
+    {
+      name: "Justin Bieber",
+      occupation: "Singer"
+    },
+    {
+      name: "Vladimir Putin",
+      occupation: "Politician"
+    },
+    {
+      name: "Oprah",
+      occupation: "Entertainer"
+    }
+  ]
+
+// // 1. Write a loop that will print out all the names of the people of the people array
+
+// for (var i = 0; i < peopleArray.length; i++){
+//     console.log(peopleArray[i].name)
+// }
+
+// how to put the names into an array: 
+
+// var names = []
+
+// for (var i = 0; i < peopleArray.length; i++){
+//     names.push(peopleArray[i].name)
+// }
+// console.log(names)
+
+// 2. Write a loop that pushes the names into a names array, and the occupations into an occupations array.
+
+// var names = []
+// var occupations = []
+
+// for (var i = 0; i < peopleArray.length; i++){
+//     names.push(peopleArray[i].name)
+//     occupations.push(peopleArray[i].occupation)
+// }
+
+// console.log(names)
+// console.log(occupations)
+
+// Write a loop that pushes every other name to an array starting with the first person, 
+// in this case "Harrison Ford", and every other occupation to another array starting with, in this case, "Singer".
+
+var everyOtherName = []
+var everyOtherOcc = []
+
+for (var i = 0; i < peopleArray.length; i++){
+    if (i % 2 === 0){
+        everyOtherName.push(peopleArray[i].name)
+    }
+}
+console.log(everyOtherName)
+
+for (var i = 1; i < peopleArray.length; i++){
+    if(i % 2 === 1){
+        everyOtherOcc.push(peopleArray[i].occupation)
+    }
+}
+
+console.log(everyOtherOcc)
