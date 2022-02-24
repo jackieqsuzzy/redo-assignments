93e90eb7095fe56f5aff76aeb08d9dd4d6eda8bf
jackieqsuzzy <61442030+jackieqsuzzy@users.noreply.github.com>
2022-02-23 21:06:59 -0700

    finished assignment

---
+++ level-1/string-methods-exercise/app.js
@@ -0,0 +1,55 @@
+// Write a function that takes a string as a parameter and 
+// returns the same string in all capital letters followed by all lowercase letters.
+// var word = "hello"
+// var toUpperCase = word.toUpperCase()
+
+// var word = "HELLO"
+// var toLowerCase = word.toLowerCase()
+
+// console.log(toUpperCase)
+// console.log(toLowerCase)
+
+
+
+// function toUpCase(str){
+//     return str.toUpperCase()
+    
+// }
+// console.log(toUpCase("hello"))
+
+// Write a function that takes a string as a parameter and returns a number 
+// that is half the string's length, rounded down.
+
+//  function findMiddleIndex(str){
+//      return Math.floor(str.length / 2)
+//  }
+//  console.log(findMiddleIndex("hello yellow"))
+
+// Write a function that uses slice() and the other functions you've 
+// written to return the first half of the given string.
+
+// function sliceItUp(str){
+//     return str.slice(0, 5)
+// }
+// console.log(sliceItUp("hello World"))
+
+// Write a function that takes a string as a parameter and returns that 
+// string where the first half is capitalized, and the second half is lowercase.
+
+// var toUpper = "hello world"
+// var strLength = toUpper.length
+// var changedStr = ""
+
+// for (var i = 0; i < Math.floor(strLength / 2); i++){
+//     changedStr += toUpper[i].toUpperCase()
+// }
+
+// for(var i=Math.floor(strLength/2) ; i < strLength ; i++)
+// {
+//     changedStr += toUpper[i];
+// }
+
+// console.log(changedStr)
+
+
+
